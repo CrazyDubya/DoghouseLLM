4c6c5b6a88bf8230dd7b0643982cc0df5c29936e
puppuccino <crazydubya@gmail.com>
2024-01-19 00:52:59 -0500

    Initial Commit

---
+++ DoghouseLLM/DoghouseLLM.h
@@ -0,0 +1,18 @@
+//
+//  DoghouseLLM.h
+//  DoghouseLLM
+//
+//  Created by Stephen Thompson on 1/19/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DoghouseLLM.
+FOUNDATION_EXPORT double DoghouseLLMVersionNumber;
+
+//! Project version string for DoghouseLLM.
+FOUNDATION_EXPORT const unsigned char DoghouseLLMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoghouseLLM/PublicHeader.h>
+
+
